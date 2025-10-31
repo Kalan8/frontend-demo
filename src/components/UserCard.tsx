efd105c10ff6a7661c3d1eedffebbe69193bd03c
Thomas M <t.moesse@gmail.com>
2025-10-31 15:59:13 +1100

    Renaming UserCard typo

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -3,13 +3,14 @@ import React from "react";
 type User = {
   id: number;
   name: string;
+  surname: string;
   email: string;
 };
 
 export default function UserCard({ user }: { user: User }) {
   return (
     <div className="bg-white shadow rounded-2xl p-4 border border-gray-200 hover:shadow-md transition">
-      <h2 className="text-lg font-semibold text-gray-800">{user.name}</h2>
+      <h2 className="text-lg font-semibold text-gray-800">{user.name} {user.surname}</h2>
       <p className="text-gray-500">{user.email}</p>
     </div>
   );
